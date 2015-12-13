f9386ad066ee1eb2c3bde541cc812cb2cb1dc7a8
fkromer <fkromer@gmail.com>
2015-12-13 12:24:51 +0100

    add: - find_and_convert() changed: - find_dot_files() file extension check (*.gv) added

--- p3/p3/pre_builder.py
+++ p3/p3/pre_builder.py
@@ -15,8 +15,8 @@ class PreBuilder():
         """
         Finds all files fitting the pattern "*.gv" within a directory recursively.
 
-        :param: dir: directory to be searched in
-        :return: files: list of dot files
+        :param dir: directory to be searched in
+        :return files: list of dot files
         """
         files = []
 
@@ -27,8 +27,9 @@ class PreBuilder():
 
             # print path to all filenames.
             for filename in filenames:
-                #print(os.path.join(dirname, filename))
-                files.append(os.path.join(dirname, filename))
+                if filename.endswith('.gv'):
+                    #print(os.path.join(dirname, filename))
+                    files.append(os.path.join(dirname, filename))
         return files
 
     def convert_to_svg(self, dot_file):
@@ -37,13 +38,27 @@ class PreBuilder():
         installation of graphviz and 
 
         :param dot_file: dot file to be converted
-        :return: svg_file: generated svg file
+        :return svg_file: generated svg file
         """
         gv_file = dot_file
         file_name = os.path.splitext(gv_file)[0]
         svg_file = file_name + '.svg'
         subprocess.call(["dot", "-Tsvg", "{}".format(gv_file), "-o", "{}".format(svg_file)])
 
+    def find_and_convert(self, dir):
+        """
+        Finds all dot input files (*.gv) recursively in a directory and converts
+        it to dot output files (*.svg).
+
+        :param dir: the directory the processing shall be applied to
+        """
+        dot_files = self.find_dot_files(dir)
+        for f in dot_files:
+            try:
+                self.convert_to_svg(f)
+            except Exception as e:
+                print('something went wrong during conversion')
+                print(e)
 
 if __name__ == "__main__":
     print('not implemented yet, testing in progress')
