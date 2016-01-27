5ecd1ec73fd51b0ad668d84b3ddcaaee7c65b50c
fkromer <thinwybk@mailbox.org>
2016-01-27 19:21:10 +0100

    add: sphinxcontrib-p3, add extension reference in conf.py

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('exts/sphinxcontrib_p3/sphinxcontrib'))
 
 # -- General configuration ------------------------------------------------
 
@@ -34,6 +35,7 @@ needs_sphinx = '1.3'
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.graphviz',
+    'p3'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
