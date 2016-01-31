1219d9823c6350d4f371dadf5fa678b167530fa8
fkromer <thinwybk@mailbox.org>
2016-01-31 18:31:23 +0100

    add: ignore extension test sphinx input files

--- conf.py
+++ conf.py
@@ -81,7 +81,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'venv*', 'p3*']
+exclude_patterns = ['_build', 'venv*', 'p3*', 'exts/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
